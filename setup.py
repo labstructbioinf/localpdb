0f1add00291d1b02997bcae1aee9a85b2e94b606
Jan Ludwiczak <j.ludwiczak@cent.uw.edu.pl>
2020-05-15 01:38:30 +0200

    Add preliminary setup script and requirements

---
+++ setup.py
@@ -0,0 +1,19 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+install_reqs = ['setuptools']
+
+setup(name='localpdb',
+      version=VERSION,
+      description='Python package for handling a local copy of the PDB database and auxiliary files',
+      author='Jan Ludwiczak',
+      author_email='j.ludwiczak@cent.uw.edu.pl',
+      url='https://github.com/labstructbioinf/localpdb',
+      packages=find_packages(),
+      install_requires=install_reqs,
+      scripts=['bin/localpdb_setup.py', 'bin/localpdb_update.py'],
+      package_data={'localpdb.utils': ['config.yml']},
+      include_package_data=True,
+      )
