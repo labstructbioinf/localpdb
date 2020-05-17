00888b5b4b37bb28b6b6be558e077f37ef269c3c
Jan Ludwiczak <j.ludwiczak@cent.uw.edu.pl>
2020-05-17 23:54:53 +0200

    Update setup workflow, prepare for pypi release

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ import os
 from setuptools import setup, find_packages
 
 VERSION = '0.1'
-install_reqs = ['setuptools']
+with open('requirements.txt') as f:
+      install_reqs = f.read().splitlines()
 
 setup(name='localpdb',
       version=VERSION,
@@ -13,7 +14,9 @@ setup(name='localpdb',
       url='https://github.com/labstructbioinf/localpdb',
       packages=find_packages(),
       install_requires=install_reqs,
+      python_requires=">=3.6.1",
       scripts=['bin/localpdb_setup.py', 'bin/localpdb_update.py'],
-      package_data={'localpdb.utils': ['remote_sources.yml']},
+      package_data={'localpdb.utils': ['remote_sources.yml'],
+                    '': ['requirements.txt']},
       include_package_data=True,
       )
