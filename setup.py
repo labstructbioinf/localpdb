87497de0fa3cd0c41df76aa334ef2e2929545d29
Jan Ludwiczak <j.ludwiczak@cent.uw.edu.pl>
2020-05-25 17:59:48 +0200

    v0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = '0.1'
+VERSION = '0.1.0'
 with open('requirements.txt') as f:
       install_reqs = f.read().splitlines()
 
