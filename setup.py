25985852a4b42f3588afb111d47749856cdd3f61
Jan Ludwiczak <j.ludwiczak@cent.uw.edu.pl>
2020-05-25 17:47:15 +0200

    Restructure requirements and setup script

--- setup.py
+++ setup.py
@@ -2,21 +2,32 @@ import io
 import os
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 VERSION = '0.1'
 with open('requirements.txt') as f:
       install_reqs = f.read().splitlines()
 
 setup(name='localpdb',
       version=VERSION,
-      description='Python package for handling a local copy of the PDB database and auxiliary files',
+      description='Store, manage and query the local copy of the PDB (Protein Data Bank) resources.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Jan Ludwiczak',
       author_email='j.ludwiczak@cent.uw.edu.pl',
       url='https://github.com/labstructbioinf/localpdb',
       packages=find_packages(),
       install_requires=install_reqs,
       python_requires=">=3.6.1",
+      extras_require={'dev': ['pytest>=4.0', 'pytest-dependency>=0.3', 'pytest_cov>=2.8']},
       scripts=['bin/localpdb_setup.py', 'bin/localpdb_update.py'],
       package_data={'localpdb.utils': ['remote_sources.yml'],
-                    '': ['requirements.txt']},
+                    '': ['requirements.txt'],
+                    'tests': ['test_config.yml']},
       include_package_data=True,
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+      ],
       )
