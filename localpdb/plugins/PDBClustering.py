8c2bac362121f78e35054116c0dc7d0289b35888
Jan Ludwiczak <j.ludwiczak@cent.uw.edu.pl>
2022-08-23 22:35:33 +0200

    Fix PDBClustering plugin issues connected with changes in RCSB

--- localpdb/plugins/PDBClustering.py
+++ localpdb/plugins/PDBClustering.py
@@ -1,5 +1,9 @@
 import logging
 import os
+import requests
+import json
+import numpy as np
+from tqdm import tqdm
 from .Plugin import Plugin
 from localpdb.utils.config import Config
 from localpdb.utils.os import create_directory
@@ -35,8 +39,16 @@ class PDBClustering(Plugin):
         clust_redundancy = [30, 40, 50, 70, 90, 95, 100]
         for redundancy in clust_redundancy:
             local_fn = f'{self.plugin_dir}/{self.plugin_version}/bc-{redundancy}.out'
-            url = self.plugin_config['clust_url'] + f'bc-{redundancy}.out'
+            url = self.plugin_config['clust_url'] + f'clusters-by-entity-{redundancy}.txt'
             download_url(url, local_fn)
+
+        entity_instance_mapping = {}
+        local_fn = f'{self.plugin_dir}/{self.plugin_version}/mapping.json'
+        entries = [idx.upper() for idx in self.lpdb.entries.index]
+        for batch in tqdm(np.array_split(entries, 250)):
+            entity_instance_mapping.update(fetch_entity_instance_mapping(batch))
+        with open(local_fn, 'w') as f:
+            f.write(json.dumps(entity_instance_mapping, indent=4))
 
     def _reset(self):
         self._load()
@@ -46,10 +58,28 @@ class PDBClustering(Plugin):
         if not str(redundancy) in valid_cutoffs:
             cutoffs = ', '.join(valid_cutoffs)
             raise ValueError(f'Cutoff \'{redundancy}\' is not a valid PDB clustering cutoff! Use any of the following: {cutoffs}')
+        fn_mapping = f'{self.plugin_dir}/{self.plugin_version}/mapping.json'
         fn = f'{self.plugin_dir}/{self.plugin_version}/bc-{redundancy}.out'
         _, pdb_idxs = self.lpdb._get_current_indexes()
-        cluster_data_filt = {key: value for key, value in parse_cluster_data(fn).items() if key in pdb_idxs}
+        if os.path.isfile(fn_mapping):
+            initial_clusters = parse_cluster_data(fn)
+            with open(fn_mapping) as f:
+                mapping = json.loads(f.read())
+                mapping_corr = {key: value.lower() for key, value in mapping.items()}
+            cluster_data_filt = {key: initial_clusters.get(mapping_corr.get(key, None), None) for key in pdb_idxs}
+        else:
+            cluster_data_filt = {key: value for key, value in parse_cluster_data(fn).items() if key in pdb_idxs}
         self.lpdb._add_col_chains(cluster_data_filt, [f'clust-{redundancy}'])
+
+
+def fetch_entity_instance_mapping(entries):
+    query = """{entries(entry_ids: %s) {polymer_entities {rcsb_id, polymer_entity_instances {rcsb_id}}}}""" % json.dumps(
+        list(entries))
+    res = requests.post("https://data.rcsb.org/graphql", json={"query": query}).json()['data']['entries']
+    data = {'{}_{}'.format(chain['rcsb_id'].split('.')[0].lower(), chain['rcsb_id'].split('.')[1]): ent['rcsb_id']
+            for r in res for ent in r['polymer_entities'] for chain in ent['polymer_entity_instances']}
+    return data
+
 
 def parse_cluster_data(fn):
     """
